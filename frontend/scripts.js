3424b3a475de21828eb2277df42c069eb93a406b
Breno Melo <bigm@discente.ifpe.edu.br>
2024-07-03 02:44:05 -0300

    changing local host

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -88,7 +88,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-u2gs.onrender.com")
     websocket.onmessage = processMessage
 }
 
