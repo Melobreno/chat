566e94beb1746e6e0bbf67ea1d2ce72495767b79
Breno Melo <bigm@discente.ifpe.edu.br>
2024-07-03 02:09:41 -0300

    feat: formatação html e css

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,8 +8,11 @@ const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 //Caso o servidor reconheça que uma mensagem foi enviada notifica os usuários
 wss.on('connection', (ws)=> {
     ws.on('error', console.error)
+
     ws.on('message', (data)=>{        
+        console.log(data.toString())
         wss.clients.forEach((client)=> client.send(data.toString()))
     })
+
     console.log("Client Connected")
 })
